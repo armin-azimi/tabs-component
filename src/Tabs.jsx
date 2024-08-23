729395eda09bbd603311dc3532b4f82166665ea2
Armin Azimi <armin.azimi1@gmail.com>
2024-08-24 00:04:28 +0330

    Tabs component finished

--- src/Tabs.jsx
+++ src/Tabs.jsx
@@ -1,8 +1,8 @@
 import './Tabs.css';
 
 const tabContent = [
-  { title: 'Tab 1', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
-  { title: 'Tab 2', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+  { title: 'Tab 1', description: '>Lorem ipsum dolor sit amet consectetur adipisicing elit. A esse ullam rerum culpa, eos ipsa modi corporis rem, sint architecto dignissimos officia blanditiis facere tempora explicabo repudiandae, aliquam ducimus totam?' },
+  { title: 'Tab 2', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. >Lorem ipsum dolor sit amet consectetur adipisicing elit. A esse ullam rerum culpa, eos ipsa modi corporis rem, sint architecto dignissimos officia blanditiis facere tempora explicabo repudiandae, aliquam ducimus totam? >Lorem ipsum dolor sit amet consectetur adipisicing elit. A esse ullam rerum culpa, eos ipsa modi corporis rem, sint architecto dignissimos officia blanditiis facere tempora explicabo repudiandae, aliquam ducimus totam?' },
   { title: 'Tab 3', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.' },
   { title: 'Tab 4', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
 ];
